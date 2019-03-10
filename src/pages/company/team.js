a177fc0a24265edb27a63573398e69006908920d
Mark Lopresto <mark@spyderhausdesign.com>
2019-03-10 10:39:21 -0400

    add company folder and team page…
    add company folder and team page. import React. import Layout. Create Team function with <div> as argument. export Team

---
+++ src/pages/company/team.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import Layout from "../../components/layout"
+
+const Team = () => (
+  <Layout>
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Nulla vitae elit libero, a pharetra augue. Maecenas sed diam eget risus
+        varius blandit sit amet non magna. Cras mattis consectetur purus sit
+        amet fermentum. Sed posuere consectetur est at lobortis. Sed posuere
+        consectetur est at lobortis. Donec id elit non mi porta gravida at eget
+        metus.
+      </p>
+    </div>
+  </Layout>
+)
+
+export default Team
