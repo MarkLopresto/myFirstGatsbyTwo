98da339ed32670765482891d05319621e3d62877
Mark Lopresto <mark@spyderhausdesign.com>
2019-03-10 12:25:28 -0400

    update title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My First Gatsby Two`,
+    description: `My second attempt at the LevelUps tutorial, but this time taking better notes`,
+    author: `marklopresto`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
