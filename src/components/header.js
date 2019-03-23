57eace4d65254f7b57e98dbf5b117195ab3a505f
Mark Lopresto <mark@spyderhausdesign.com>
2019-03-23 10:13:09 -0400

    begin to customize header file
    add link and styled components. remove logo and prop-types. remove inline styling from default starter. begin to experiment with nesting and using CSS Grid, here.

--- src/components/header.js
+++ src/components/header.js
@@ -1,46 +1,72 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 
-import gatsbyLogo from '../images/gatsby-icon.png'
+import { Link } from "gatsby"
+import styled from "styled-components"
+
+const SectionHeader = styled.section`
+  background-color: #121421;
+  padding: 3rem 1rem;
+`;
+
+const SectionHeaderContainer = styled.div`
+  align-items: center;
+  display: grid;
+  grid-template:
+    "logo ."
+    ". subhead1"
+    "hline hline"
+    ". subhead2";
+    justify-content: space-between;
+  margin: 0 auto;
+  max-width: 1024px;
+
+  img {
+    grid-area: logo;
+    max-width: 100%;
+    height: auto;
+  }
+
+  h1 {
+    color: #fefefe;
+    font-size: 4rem;
+  }
+
+  h4 {
+    color: #fefefe;
+    margin: 0;
+    &:first-of-type {
+      grid-area: subhead1;
+      margin-bottom: .5rem;
+    }
+
+    &:last-of-type {
+      grid-area: subhead2;
+      margin-top: .5rem;
+      text-align: right;
+    }
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  .h-line {
+    grid-area: hline;
+    border: 2px solid #fefefe;
+    height: 2px;
+    /* max-width: 96vw; */
+  }
+`;
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          <img style={{
-            width: `100px`,
-          }} src={gatsbyLogo} alt="Gatsby Logo"/>
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <SectionHeader>
+    <SectionHeaderContainer>
+        <Link to="/"><h1>Mark Lopresto</h1></Link>
+        <h4>design[er] + develop[er] + deliver[er]</h4>
+        <div className="h-line"></div>
+        <h4>Cleveland OH + Beyond</h4>
+    </SectionHeaderContainer>
+  </SectionHeader>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
