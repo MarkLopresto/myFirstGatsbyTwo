f8f675022535b73ab4134b62bf58d2ffd1f2a67c
Mark Lopresto <mark@spyderhausdesign.com>
2019-03-24 08:39:33 -0400

    remove inline styles and…
    remove inline styles. change main to section wrapping children. use MainLayout as main

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,22 +35,15 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
+        <MainLayout>
+          <section>{children}</section>
           <Archive />
           <footer>
             © {new Date().getFullYear()}, Built with
             {` `}
             <a href="https://www.gatsbyjs.org">Gatsby</a>
           </footer>
-        </div>
+        </MainLayout>
       </>
     )}
   />
